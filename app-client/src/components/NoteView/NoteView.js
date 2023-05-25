1c5e12b002b5d9ccef7d0eaee27dc6959e5980b3
Jakub Perron <lightninglie2@gmail.com>
2023-05-25 20:31:58 +0200

    poprawa wizualna czy cos

--- app-client/src/components/NoteView/NoteView.js
+++ app-client/src/components/NoteView/NoteView.js
@@ -109,21 +109,23 @@ export default function NoteView(props) {
             <div id="title">Edytuj zadanie</div>
             <div id="frima">
                 <form>
-                    Tytuł:<br></br>
+                    <div>
+                        <br></br>Tytuł i Priorytet<br></br>
                     <input id="tytul" value={title} onChange={e => setTitle(e.target.value)}/>
 
-                    Priorytet:
-                    <select id="select1" value={priority} onChange={(e) => setPriority(e.target.value) }>
+                    
+                    <select id="prio" value={priority} onChange={(e) => setPriority(e.target.value) }>
                         <option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
-                    </select><br></br>
+                    </select><br></br><br></br>
+                    </div>
 
-                    Sale:<br></br>
-                    <select id="rooms" value={rooms} onChange={(e) => setRooms(e.target.value)} >
+                    <div>
+                        Wybierz sale i osobe<br></br>
+                    <select id="select1" value={rooms} onChange={(e) => setRooms(e.target.value)} >
                         <option>2</option><option>3</option><option>4</option><option>105</option><option>109</option><option>117</option><option>121</option><option>Serwerownia</option>
                     </select>
 
 
-                    Wybierz osobe<br></br>
                     
                     {users.length > 0 && (
                         <select id="select1" onChange={(e) => {
@@ -138,16 +140,19 @@ export default function NoteView(props) {
                                 </option>
                             ))}
                         </select>
-                    )}
-
-                    Data dodania:
+                    )}<br></br><br></br>
+                    </div>
+                    <div>
+                        Data dodania i  Deadline:
                     <input type="date" id="data" value={date} onChange={(e) => setDateAdded(e.target.value)} />
 
-                    Deadline:
-                    <input type="date" id="data" value={deadline} onChange={(e) => setDeadline(e.target.value)}/>
-
-                    Opis:
+                    
+                        <input type="date" id="data" value={deadline} onChange={(e) => setDeadline(e.target.value)} /><br></br><br></br>
+                    </div>
+                    <div>
+                    Opis:<br></br>
                     <textarea id="opis" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                    </div>
                 </form>
                 <Buttons viewMode={props.viewMode} handleClickAdd={handleClickAdd} handleClickModify={handleClickModify} handleClickDelete={handleClickDelete} />
             </div>
